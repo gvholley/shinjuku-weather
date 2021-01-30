ea56c1f865ff081be6617328780de6966b52ecc1
Grant Holley <gvholley@gmail.com>
2021-01-30 19:15:43 +0900

    Added React Weather

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import { Container } from 'react-bootstrap';
 import useFetch from './hooks/useFetch';
 import {apiKey, baseUrl} from './config';
 import WeatherList from './components/WeatherList';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
 
 function App() {
 
   const { data, error, inProgress, setUrl } = useFetch();
   return (
     <Container className="App">
-      <CitySelector onSelectButtonClick={city => setUrl(`${baseUrl}/data/2.5/forecast/daily?q=${city}&cnt=5&appId=${apiKey}&units=metric`)} />
+      <CitySelector onSelectButtonClick={city => setUrl(`https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=${apiKey}&units=metric`)} />
       {data && <WeatherList weathers={data.list} />}
     </Container>
   );
