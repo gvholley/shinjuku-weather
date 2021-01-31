43444af3d6e074d0a6236f1ea6e113507325ff8b
Grant Holley <gvholley@gmail.com>
2021-01-31 21:47:57 +0900

    Editing week container

--- src/components/WeekContainer.js
+++ src/components/WeekContainer.js
@@ -4,19 +4,30 @@ class WeekContainer extends React.Component {
   render() {
 
     componentDidMount = () => {
-    const weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=35.6896&lon=139.7006&appid=e6c5ffe9d357a02f1df56eaa546d46dc"
+    const weatherURL =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=35.6896&lon=139.7006&appid=e6c5ffe9d357a02f1df56eaa546d46dc"
 
-    fetch(weatherUrl)
-    .then( res => res.json())
-    .then( data => console.log("DataList Loaded", data.list))
+  fetch(weatherURL)
+    .then(res => res.json())
+    .then(data => {
+      const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+      this.setState({
+        fullData: data.list,
+        dailyData: dailyData
+      }, () => console.log(this.state))
+    })
   }
 
+  formatDayCards = () => {
+    return this.state.dailyData.map((reading, index) => <DayCard reading={reading} key={index} />)
+  }
+
+  render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        {this.formatDayCards()}
       </div>
     )
   }
-}
 
 export default WeekContainer;
